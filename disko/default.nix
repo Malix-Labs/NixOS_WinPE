69ff8f6930a5b8ac69b5181b783ec2ebf5c475f0
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-08-23 14:52:10 +0200

    refactor(disko): use mapAttrsRecursive for clean declarative partition defaults

--- disko/default.nix
+++ disko/default.nix
@@ -1,20 +1,23 @@
 { lib, ... }:
-{
-  # Standalone Disko partition snippet for WinPE.
-  # Can be imported directly into disko.devices.disk.<name>.content.partitions
-  disko.devices.disk.main.content.partitions.WinPE = {
-    priority = lib.mkDefault 2;
-    size = lib.mkDefault "2G";
-    type = lib.mkDefault "0700"; # Microsoft Basic Data / FAT32
+let
+  partition = {
+    priority = 2;
+    size = "2G";
+    type = "0700";
     content = {
-      type = lib.mkDefault "filesystem";
-      format = lib.mkDefault "vfat";
-      mountpoint = lib.mkDefault "/mnt/WinPE";
-      mountOptions = lib.mkDefault [
+      type = "filesystem";
+      format = "vfat";
+      mountpoint = "/mnt/WinPE";
+      mountOptions = [
         "nofail"
         "fmask=0077"
         "dmask=0077"
       ];
     };
   };
+in
+{
+  disko.devices.disk.main.content.partitions.WinPE = lib.mapAttrsRecursive (
+    _: lib.mkDefault
+  ) partition;
 }
