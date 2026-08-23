bb3b32f6535ee5bab078d74348123116452cabc3
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-08-23 13:42:09 +0200

    refactor(profile): remove experimental udev rules in favor of upstream nixos-hardware

--- profiles/lenovo-legion-15ach6h.nix
+++ profiles/lenovo-legion-15ach6h.nix
@@ -1,21 +1,15 @@
 {
-  config,
   lib,
   pkgs,
   ...
 }:
 let
-  cfg = config.hardware.winpe.profiles.lenovoLegion;
   biosPackage = pkgs.callPackage ../pkgs/lenovo-legion-bios { };
 in
 {
   imports = [
     ../nixos
   ];
-
-  options.hardware.winpe.profiles.lenovoLegion = {
-    enableUdevRules = lib.mkEnableOption "experimental udev access rules for the ITE 8910 keyboard controller";
-  };
 
   config = {
     hardware.winpe = {
@@ -31,10 +25,5 @@ in
         ];
       };
     };
-
-    services.udev.extraRules = lib.mkIf cfg.enableUdevRules ''
-      SUBSYSTEM=="usb", ATTR{idVendor}=="048d", ATTR{idProduct}=="c965", MODE="0666", TAG+="uaccess"
-      SUBSYSTEM=="usb", ATTR{idVendor}=="048d", ATTR{idProduct}=="c101", MODE="0666", TAG+="uaccess"
-    '';
   };
 }
